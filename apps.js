33e4d2d5dd0775ff820f7a0a6f96e38dfd4f62ad
Atekaalsheyab <atekaalsheyab22@gmail.com>
2023-02-12 06:25:35 +0000

    a change was added

--- apps.js
+++ apps.js
@@ -49,25 +49,20 @@ let empArr= new Array();
 
 let emp1= new Employee(1000, "Ghazi Samer", "Administration", "Senior", ""); 
 emp1.calculateSalary(this.Level); 
-empArr[0]=emp1; 
 let emp2= new Employee(1001, "Lana Ali", "Finance", "Senior", ""); 
 emp2.calculateSalary(this.Level); 
-empArr[1]=emp2; 
 let emp3= new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", ""); 
 emp3.calculateSalary(this.Level); 
-empArr[2]=emp3; 
 let emp4= new Employee(1003, "Safi Walid", "Administration", "Mid-Senior", ""); 
 emp4.calculateSalary(this.Level); 
-empArr[3]=emp4; 
 let emp5= new Employee(1004, "Omar Zaid", "Development", "Senior", ""); 
 emp5.calculateSalary(this.Level); 
-empArr[4]=emp5; 
 let emp6= new Employee(1005, "Rana Saleh", "Development", "Junior", ""); 
 emp6.calculateSalary(this.Level); 
-empArr[5]=emp6; 
 let emp7= new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", ""); 
 emp7.calculateSalary(this.Level); 
-empArr[6]=emp7; 
+
+empArr=[emp1,emp2,emp3,emp4,emp5,emp6,emp7];
 
 // emp1.render(); 
 
