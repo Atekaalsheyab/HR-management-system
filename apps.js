343abf711035008f134c4a7d06dc0f7ae520b9d0
Atekaalsheyab <atekaalsheyab22@gmail.com>
2023-02-21 11:44:53 +0000

    add new html elements and js function

--- apps.js
+++ apps.js
@@ -1,30 +1,30 @@
 "use strict"
-
-function Employee(EmployeeId, FullName, Department, Level, Image_URL, Salary) {
-    this.EmployeeId = EmployeeId;
+let max;
+let min;
+function Employee( FullName, Department, Level, Image_URL, Salary) {
+    this.EmployeeId = this.generateId;
     this.FullName = FullName;
     this.Department = Department;
     this.Level = Level;
     this.Image_URL = Image_URL;
-    this.Salary = 0;
+    this.Salary = this.calculateSalary;
 }
 
 Employee.prototype.calculateSalary = function () {
 
-    let max;
-    let min;
+
     switch (this.Level) {
         case "Senior":
             max = 2000;
             min = 1500;
             this.Salary = Math.floor(Math.random() * (max - min) + min);
-           return  this.Salary = this.Salary - ((7.5 / 100) * this.Salary);
+            return this.Salary = this.Salary - ((7.5 / 100) * this.Salary);
             break;
         case "Mid-Senior":
             max = 1500;
             min = 1000;
             this.Salary = Math.floor(Math.random() * (max - min) + min);
-           return  this.Salary = this.Salary - ((7.5 / 100) * this.Salary);
+            return this.Salary = this.Salary - ((7.5 / 100) * this.Salary);
             break;
         case "Junior":
             max = 500;
@@ -38,36 +38,56 @@ Employee.prototype.calculateSalary = function () {
     }
 }
 
-
-Employee.prototype.render = function () {
-    document.write( `EmployeeName : ${this.FullName}    Salary:  ${this.Salary}`); 
-    document.write("<br>");
+Employee.prototype.generateId = function () {
+    min = 1000; 
+    max =9999;
+   
+    this.EmployeeId= Math.floor(Math.random() * (max - min) + min);
 
 }
 
-let empArr= new Array();
+Employee.prototype.render = function () {
+   const container= document.getElementById('render'); 
 
-let emp1= new Employee(1000, "Ghazi Samer", "Administration", "Senior", ""); 
-emp1.calculateSalary(this.Level); 
-let emp2= new Employee(1001, "Lana Ali", "Finance", "Senior", ""); 
-emp2.calculateSalary(this.Level); 
-let emp3= new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", ""); 
-emp3.calculateSalary(this.Level); 
-let emp4= new Employee(1003, "Safi Walid", "Administration", "Mid-Senior", ""); 
-emp4.calculateSalary(this.Level); 
-let emp5= new Employee(1004, "Omar Zaid", "Development", "Senior", ""); 
-emp5.calculateSalary(this.Level); 
-let emp6= new Employee(1005, "Rana Saleh", "Development", "Junior", ""); 
-emp6.calculateSalary(this.Level); 
-let emp7= new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", ""); 
-emp7.calculateSalary(this.Level); 
+   const divEl1= document.createElement('div'); 
+   container.appendChild(divEl1);
+   const divEl2= document.createElement('div'); 
+   container.appendChild(divEl2);
+   const divEl3= document.createElement('div'); 
+   container.appendChild(divEl3);
+}
 
-empArr=[emp1,emp2,emp3,emp4,emp5,emp6,emp7];
+let empArr = new Array();
+
+let emp1 = new Employee("Ghazi Samer", "Administration", "Senior", "");
+emp1.generateId(); 
+emp1.calculateSalary(this.Level);
+let emp2 = new Employee("Lana Ali", "Finance", "Senior", "");
+emp2.generateId();
+emp2.calculateSalary(this.Level);
+let emp3 = new Employee("Tamara Ayoub", "Marketing", "Senior", "");
+emp3.generateId(); 
+emp3.calculateSalary(this.Level);
+let emp4 = new Employee("Safi Walid", "Administration", "Mid-Senior", "");
+emp4.generateId(); 
+emp4.calculateSalary(this.Level);
+let emp5 = new Employee("Omar Zaid", "Development", "Senior", "");
+emp5.generateId();
+emp5.calculateSalary(this.Level);
+let emp6 = new Employee("Rana Saleh", "Development", "Junior", "");
+emp6.generateId();
+emp6.calculateSalary(this.Level);
+let emp7 = new Employee("Hadi Ahmad", "Finance", "Mid-Senior", "");
+emp7.calculateSalary(this.Level);
+emp7.generateId();
+
+empArr = [emp1, emp2, emp3, emp4, emp5, emp6, emp7];
 
 // emp1.render(); 
 
-for (let index = 0; index <empArr.length; index++) {
-    empArr[index].render(); 
-    
-}
+// for (let index = 0; index < empArr.length; index++) {
+//     empArr[index].render();
 
+// }
+
+emp1.render(); 
