a312ac54b62203da63a1112dbe2e3d1b10767873
Atekaalsheyab <atekaalsheyab22@gmail.com>
2023-02-22 13:30:25 +0000

    an accounting js file was added

---
+++ accounting.js
@@ -0,0 +1,48 @@
+'use strict'
+// import {EmpArr} from "./apps";
+// import{AvgAdmin, AvgDev, AvgFin, AvgMar, AdminCount,MarCount,FinCount,DevCount} from "./apps";
+
+const cont = document.getElementById('body-table'); 
+const trEl1= document.createElement('tr'); 
+cont.appendChild(trEl1); 
+const tdEl1= document.createElement('td'); 
+trEl1.appendChild(tdEl1); 
+const tdEl2= document.createElement('td'); 
+trEl1.appendChild(tdEl2); 
+const tdEl3= document.createElement('td'); 
+trEl1.appendChild(tdEl3); 
+const tdEl4= document.createElement('td'); 
+trEl1.appendChild(tdEl4); 
+
+const trEl2= document.createElement('tr'); 
+cont.appendChild(trEl2); 
+const tdEl21= document.createElement('td'); 
+trEl2.appendChild(tdEl21); 
+const tdEl22= document.createElement('td'); 
+trEl2.appendChild(tdEl22); 
+const tdEl23= document.createElement('td'); 
+trEl2.appendChild(tdEl23); 
+const tdEl24= document.createElement('td'); 
+trEl2.appendChild(tdEl24); 
+
+const trEl3= document.createElement('tr'); 
+cont.appendChild(trEl3); 
+const tdEl31= document.createElement('td'); 
+trEl3.appendChild(tdEl31); 
+const tdEl32= document.createElement('td'); 
+trEl3.appendChild(tdEl32); 
+const tdEl33= document.createElement('td'); 
+trEl3.appendChild(tdEl33); 
+const tdEl34= document.createElement('td'); 
+trEl3.appendChild(tdEl34); 
+
+const trEl4= document.createElement('tr'); 
+cont.appendChild(trEl4); 
+const tdEl41= document.createElement('td'); 
+trEl4.appendChild(tdEl41); 
+const tdEl42= document.createElement('td'); 
+trEl4.appendChild(tdEl42); 
+const tdEl43= document.createElement('td'); 
+trEl4.appendChild(tdEl43); 
+const tdEl44= document.createElement('td'); 
+trEl4.appendChild(tdEl44); 
